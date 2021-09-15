ef5405f23e8d119c00aa6fcf37237150933d10ec
@BenjaminWegener <wegeneredv@gmail.com>
2021-09-15 11:24:04 +0200

    debug

--- keras_multi_head/multi_head_attention.py
+++ keras_multi_head/multi_head_attention.py
@@ -10,26 +10,26 @@ class MultiHeadAttention(keras.layers.Layer):
     def build(self, input_shape):
         if not isinstance(input_shape, tuple):
             raise ValueError('Invalid input')
-        d_model = input_shape[-1]
-        input_size = input_shape[-2]
-        linformer_depth = input_shape[-1]
+        self.d_model = input_shape[-1]
+        self.input_size = input_shape[-2]
+        self.linformer_depth = input_shape[-1]
 
         return super().build(input_shape)
 
     def call(self, inputs, **kwargs):
         def create_projection_model():
-            inputs = keras.models.Input(shape = (input_size, linformer_depth))
-            outputs = keras.layers.Dense(linformer_depth, name = 'proj', kernel_initializer = 'glorot_normal', bias_initializer = 'glorot_normal')(inputs)
+            inputs = keras.models.Input(shape = (self.input_size, self.linformer_depth))
+            outputs = keras.layers.Dense(self.linformer_depth, name = 'proj', kernel_initializer = 'glorot_normal', bias_initializer = 'glorot_normal')(inputs)
             return keras.models.Model(inputs, outputs)
 
         projection_model = create_projection_model()
         
         def create_attention_model():
-            queries = keras.models.Input(shape = (input_size, linformer_depth))
-            keys = keras.models.Input(shape = (input_size, linformer_depth))
-            values = keras.models.Input(shape = (input_size, linformer_depth))
-            causal_mask = keras.models.Input(shape = (input_size, linformer_depth))
-            infinity_mask = keras.models.Input(shape = (input_size, linformer_depth))
+            queries = keras.models.Input(shape = (self.input_size, self.linformer_depth))
+            keys = keras.models.Input(shape = (self.input_size, self.linformer_depth))
+            values = keras.models.Input(shape = (self.input_size, self.linformer_depth))
+            causal_mask = keras.models.Input(shape = (self.input_size, self.linformer_depth))
+            infinity_mask = keras.models.Input(shape = (self.input_size, self.linformer_depth))
 
             keys_transposed = keras.layers.Permute((2, 1), name = 'keys_transposed')(keys) # 0, 64, 1024
             values_transposed = keras.layers.Permute((2, 1), name = 'values_transposed')(values) # 0, 64, 1024
@@ -40,7 +40,7 @@ class MultiHeadAttention(keras.layers.Layer):
             e_proj_transposed = keras.layers.Permute((2, 1), name = 'e_proj_transpose')(e_proj) # 0, 64, 64
             f_proj_transposed = keras.layers.Permute((2, 1), name = 'f_proj_transpose')(f_proj) # 0, 64, 64
             qk = keras.layers.Dot(name = 'qk', axes = -1)([queries, e_proj_transposed]) # 0, 1024, 64
-            p_bar = keras.layers.Lambda(lambda x: x * K.constant(1 / np.sqrt(linformer_depth)), name = 'p_bar', output_shape = lambda s: (s))(qk) # 0, 1024, 64
+            p_bar = keras.layers.Lambda(lambda x: x * K.constant(1 / np.sqrt(self.linformer_depth)), name = 'p_bar', output_shape = lambda s: (s))(qk) # 0, 1024, 64
             p_bar = keras.layers.Multiply(name = 'multiply_causal')([p_bar, causal_mask]) # 0, 1024, 64
             p_bar = keras.layers.Add(name = 'add_infinitymask')([p_bar, infinity_mask])
             p_bar = keras.layers.Activation(activation = 'softmax', name = 'softmax')(p_bar) # 0, 1024, 64
@@ -52,10 +52,10 @@ class MultiHeadAttention(keras.layers.Layer):
         attention_model = create_attention_model()
         
         
-        queries = keras.layers.Dense(linformer_depth)(inputs)
-        keys = keras.layers.Dense(linformer_depth)(inputs)
-        values = keras.layers.Dense(linformer_depth)(inputs)
-        causal_mask = keras.layers.Lambda(lambda x: K.constant(np.expand_dims(np.transpose(np.triu(np.ones([linformer_depth, input_size]), 0)), 0)), output_shape = lambda s: (None, s[1], s[2]), name = 'causal_mask')(queries) # 0, 64, 1024 #only once compute
+        queries = keras.layers.Dense(self.linformer_depth)(inputs)
+        keys = keras.layers.Dense(self.linformer_depth)(inputs)
+        values = keras.layers.Dense(self.linformer_depth)(inputs)
+        causal_mask = keras.layers.Lambda(lambda x: K.constant(np.expand_dims(np.transpose(np.triu(np.ones([self.linformer_depth, self.input_size]), 0)), 0)), output_shape = lambda s: (None, s[1], s[2]), name = 'causal_mask')(queries) # 0, 64, 1024 #only once compute
         infinity_mask = keras.layers.Lambda(lambda x: (x - 1) * 1e16, output_shape = lambda s: (None, s[1], s[2]), name = 'infinity_mask')(causal_mask) # 0, 1024, 64
         
         result = attention_model([queries, keys, values, causal_mask, infinity_mask])
